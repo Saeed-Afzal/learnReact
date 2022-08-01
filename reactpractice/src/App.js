78bdec936e0e81348b94f2ccadfd2c0223bea17b
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-08-01 17:23:58 +0500

    react basics

---
+++ reactpractice/src/App.js
@@ -0,0 +1,32 @@
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <>
+
+//     <MyName/>
+//     <h1>Hello World</h1>
+//     <p>First Project</p>
+    
+//     </>
+//   )
+// }
+// const MyName = () => {
+//   return <h1>Saeed Afzal</h1>
+// }
+
+// export default App
+
+import React from 'react'
+import  Resturant  from './components/basics/Resturant'
+
+
+const App = () => {
+  return (
+    
+      <Resturant />
+   
+  )
+}
+
+export default App
